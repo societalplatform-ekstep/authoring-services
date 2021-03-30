71ecd26f1e45417a1f8336973139b008d0315cd2
Rishabh Srivastava <rishabhsrivastav03@gmail.com>
2021-03-30 11:11:36 +0530

    Updated UpdateContentCreator request model

--- src/main/java/com/infosys/lexauthoringservices/model/neo4j/UpdateContentCreator.java
+++ src/main/java/com/infosys/lexauthoringservices/model/neo4j/UpdateContentCreator.java
@@ -1,6 +1,5 @@
 package com.infosys.lexauthoringservices.model.neo4j;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
@@ -8,8 +7,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
@@ -20,6 +17,9 @@ public class UpdateContentCreator {
     @NotBlank(message = "UserId may not be blank")
     private String userId;
 
-    @NotBlank(message = "targetUser may not be blank")
-    private String targetUser;
+    @NotBlank(message = "creatorId may not be blank")
+    private String creatorId;
+
+    @NotBlank(message = "targetCreatorId may not be blank")
+    private String targetCreatorId;
 }
