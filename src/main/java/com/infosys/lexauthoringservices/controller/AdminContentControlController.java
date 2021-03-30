5430917169fce0d30e8452c16c3a61ad37e6a730
Rishabh Srivastava <rishabhsrivastav03@gmail.com>
2021-03-30 11:33:51 +0530

    Passes rootOrg & org in headers instead of params and removed unused variables

--- src/main/java/com/infosys/lexauthoringservices/controller/AdminContentControlController.java
+++ src/main/java/com/infosys/lexauthoringservices/controller/AdminContentControlController.java
@@ -2,22 +2,26 @@ package com.infosys.lexauthoringservices.controller;
 
 import com.infosys.lexauthoringservices.model.neo4j.UpdateContentCreator;
 import com.infosys.lexauthoringservices.service.AdminContentControlService;
-import com.infosys.lexauthoringservices.util.LexConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 
 @RestController
 @RequestMapping("/v1/admin/content")
+@Validated
 public class AdminContentControlController {
     @Autowired
     AdminContentControlService adminContentControlService;
 
     @PutMapping("/update/creator")
-    public ResponseEntity<?> updateContentCreator(@RequestParam(value = LexConstants.ROOT_ORG, defaultValue = "Infosys") String rootOrg,
-                                                  @RequestParam(value = LexConstants.ORG, defaultValue = "Infosys Ltd") String org,
-                                                  @RequestBody UpdateContentCreator updateContentCreator) throws Exception {
+    public ResponseEntity<?> updateContentCreator(@NotBlank @RequestHeader String rootOrg,
+                                                  @NotBlank @RequestHeader String org,
+                                                  @Valid @RequestBody UpdateContentCreator updateContentCreator) throws Exception {
         adminContentControlService.updateContentCreator(rootOrg, org, updateContentCreator);
         return new ResponseEntity<>(HttpStatus.OK);
     }
